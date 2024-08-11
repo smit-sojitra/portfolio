d3e0b6969c65793256708bdbb08b4b7e369ee1a6
Smit Sojitra <smitsojitra810@gmail.com>
2024-08-11 23:06:02 +0530

    Update

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,5 +1,5 @@
 import next from "../assets/NextJsCircle.png"
-import react from "../assets/React.svg"
+import react1 from "../assets/React.svg"
 import mongo from "../assets/MongoDB.svg"
 import tailwind from "../assets/Tailwind.png"
 import typeScript from "../assets/Typescript.svg"
@@ -12,7 +12,6 @@ import shop from "../assets/shopeWebsite.png"
 import razorpay from "../assets/razorpay.png"
 import express from "../assets/Express-removebg-preview.png"
 import blog from "../assets/blogWebsite.png"
-import nodemailder from "../assets/nodemailer.png"
 import html from "../assets/HTML.png"
 import css from "../assets/CSS.png"
 import js from "../assets/Javascript.svg"
@@ -39,7 +38,7 @@ export const data =[
     {
         title:'Portfolio',
         desc:'A comprehensive portfolio using the MERN stack includes features like project galleries, contact forms, and a detailed about section, all managed with a robust backend and a responsive, user-friendly frontend. This portfolio highlights my proficiency in full-stack development and my ability to create engaging, performant web applications.',
-        Img:[react,tailwind,css,node,express,mongo],
+        Img:[react1,tailwind,css,node,express,mongo],
         image:portfolio,
         Llink:'#',
         Glink:'',
@@ -48,7 +47,7 @@ export const data =[
     {
         title:'Shopping Webiste',
         desc:'Shop smart with our React and Tailwind CSS-powered website, enhanced by Redux Toolkit for efficient state management. Enjoy seamless browsing, quick updates, and a personalized shopping experience with ease.',
-        Img:[react,redux,tailwind,css],
+        Img:[react1,redux,tailwind,css],
         image:shop,
         Llink:'https://shopping-website-814.netlify.app',
         Glink:'https://github.com/smit-sojitra/Shopping-Website',
@@ -56,7 +55,7 @@ export const data =[
     {
         title:'Blog Webiste',
         desc:'Explore my blog website crafted with React and Tailwind CSS, leveraging appContext for streamlined state management. Enjoy seamless navigation, responsive design, and a personalized reading experience, all tailored for optimal user engagement.',
-        Img:[react,tailwind,css],
+        Img:[react1,tailwind,css],
         image:blog,
         Llink:'https://blog-website66.netlify.app',
         Glink:'https://github.com/smit-sojitra/Blog-Website',
